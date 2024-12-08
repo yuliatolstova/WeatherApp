2786e61f041d0b652ab99bf0b248fb1fecc80366
Tolstova Yulia <yu.tolstova@minervasoft.ru>
2024-12-08 14:43:49 +0400

    add api key

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Weather app with Vite and Vue
 
 I created this app to understand how vue works.
+
+The project is available on https://weather-app-seven-flax-89.vercel.app/
--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,7 @@ export default {
     weatherSearch() {
       this.loading = true;
       this.error = false;
-      fetch(`http://api.weatherapi.com/v1/current.json?key=40b888d46c0244ed93093632240812&q=${this.searchQuery}`)
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.KEY}&q=${this.searchQuery}`)
           .then(response=>response.json())
           .then(data=>{
             this.loading = false;
